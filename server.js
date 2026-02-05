97ac88c3ddf45dc5567f5d255524c12d2a2c61eb
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-02-05 00:30:30 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "eagler-wss-proxy-template",
+  "version": "1.0.0",
+  "private": true,
+  "type": "commonjs",
+  "main": "server.js",
+  "engines": {
+    "node": ">=18"
+  },
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "ws": "^8.16.0"
+  }
+}
